4c1a3d5342ee0089f6872c6d402d471894d81823
ZYC <2026110629910427@ruc.edu.cn>
2026-06-11 10:21:03 +0800

    remove rmdb/ directory from repo

--- rmdb/build/src/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zyc/db2026/rmdb/src/test
-# Build directory: /home/zyc/db2026/rmdb/build/src/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
